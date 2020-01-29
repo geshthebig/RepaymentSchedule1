bfe651008cf98204c3fbe716e78580178e4f32c8
geshthebig <60343343+geshthebig@users.noreply.github.com>
2020-01-29 14:41:06 +0200

    Added basic HTML and some CSS

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Financing parameters</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+    <header class="content">
+        <h1> Find the best financing scheme for your investment </h1>
+        <h2> Please enter your financing parameters </h2>
+    </header>
+
+    <body>
+        <div class="container">
+            <form method="post" class="Form"> 
+                <div>    
+                    <label>Loan amount</label>
+                    <input type="text" name="loanAmount" placeholder="Enter loan amount">
+                </div> 
+
+                <div>
+                    <label>Period (months)</label>
+                    <input type="text" name="period" placeholder="Enter months of repayment" >
+                </div> 
+                    
+                <div>
+                    <label>Interest rate</label>
+                    <input type="text" name="interestRate" placeholder="Enter the rate">
+                </div> 
+                <input type="submit" value="Check" class= "button">
+            </form>
+        </div>
+    </body>
+    
+</html>
